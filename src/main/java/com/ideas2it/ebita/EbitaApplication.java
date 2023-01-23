f519724d1b243fe91d8b16406f38c55959f1e979
Selva <selvakumar.rayappan@ideas2it.com>
2023-01-23 15:19:57 +0530

    Initial Commit
    Initial Commit - Spring 3.0.2. Java 17 Maven

---
+++ src/main/java/com/ideas2it/ebita/EbitaApplication.java
@@ -0,0 +1,13 @@
+package com.ideas2it.ebita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbitaApplication.class, args);
+	}
+
+}
